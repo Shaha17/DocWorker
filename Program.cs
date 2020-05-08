c1c8f8a9c85252b74ed90471abc5c908b700f605
Шахзод Джабборов <shahzod@shah.local>
2020-05-08 13:23:24 +0500

    add DocumentWorker class

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace TestTask
+{
+	class DocumentWorker
+	{
+		public virtual void OpenDocument()
+		{
+			Console.WriteLine("Документ открыт");
+		}
+		public virtual void EditDocument()
+		{
+			Console.WriteLine("Редактирование документа доступно в версии Про");
+		}
+		public virtual void SaveDocument()
+		{
+			Console.WriteLine("Сохранение документа доступно в версии Про");
+		}
+	}
+	class ProDocumentWorker : DocumentWorker
+	{
+		public override void EditDocument()
+		{
+			Console.WriteLine("Документ отредактирован");
+		}
+		public override void SaveDocument()
+		{
+			Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+		}
+	}
+	class ExpertDocumentWorker : ProDocumentWorker
+	{
+		public override void SaveDocument()
+		{
+			Console.WriteLine("Документ сохранен в новом формате");
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input;
+			Console.WriteLine("Введите ключ доступа: ");
+			//ProDocumentWorker - pro
+			//ExpertDocumentWorker - exp
+			//DocumentWorker - else
+
+			input = Console.ReadLine().ToLower();
+
+			DocumentWorker docWorker;
+			if(input == "exp")
+				docWorker = new ExpertDocumentWorker();
+			else if(input == "pro")
+				docWorker = new ProDocumentWorker();
+			else docWorker = new DocumentWorker();
+
+			docWorker.OpenDocument();
+			docWorker.EditDocument();
+			docWorker.SaveDocument();
+		}
+	}
+}
